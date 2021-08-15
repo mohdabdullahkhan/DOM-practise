e82e2dcb62319ca2a9cfba7f818e77eb75f6e2b6
mohdabdullahkhan <abdullahalig96@gmail.com>
2021-08-15 11:00:27 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// to select using id
+let a = document.getElementById("first");
+console.log(a);
+
+// to select using class
+let b = document.getElementsByClassName("second");
+console.log(b);
+
+// to select using tag
+let c = document.getElementsByTagName("ul");
+console.log(c);
+
+// to select using query selector
+let d = document.querySelectorAll("second");
+console.log(d);
